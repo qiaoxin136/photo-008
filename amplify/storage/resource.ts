3a75d7bcfbaaeab0b7cedb9d7cbb56c559fb0ad3
Xin Qiao <qiaoxin136@gmail.com>
2026-03-23 09:33:26 -0400

    update storage requirement from AO

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,9 +5,9 @@ export const imagesStorage = defineStorage({
   name: 'images',
   access: (allow) => ({
     'originals/*': [
-      allow.authenticated.to(['write', 'read', 'delete']),
-     
-    ],
- 
+      allow.authenticated.to(['write', 'delete']),
+      allow.guest.to(['read']),          // ← add this line
+    ]
+
   })
 })
